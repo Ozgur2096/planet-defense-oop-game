a38362bcc01ab33e768312923f4f3d348b4ba4a8
Ozgur Ozhan <ozgur2096@gmail.com>
2023-12-08 13:43:21 +0100

    mousemove added

--- script.js
+++ script.js
@@ -20,9 +20,23 @@ class Game {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.planet = new Planet(this);
+
+    this.mouse = {
+      x: 0,
+      y: 0,
+    };
+
+    window.addEventListener('mousemove', e => {
+      this.mouse.x = e.offsetX;
+      this.mouse.y = e.offsetY;
+    });
   }
   render(context) {
     this.planet.draw(context);
+    context.beginPath();
+    context.moveTo(this.planet.x, this.planet.y);
+    context.lineTo(this.mouse.x, this.mouse.y);
+    context.stroke();
   }
 }
 
@@ -36,4 +50,11 @@ window.addEventListener('load', function () {
 
   const game = new Game(canvas);
   game.render(ctx);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    window.requestAnimationFrame(animate);
+  }
+  this.window.requestAnimationFrame(animate);
 });
